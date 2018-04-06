ad4a97f126138c87f92d7eaf848e49a706d41f45
Justin Comeaux <jkcomeaux22@gmail.com>
2018-04-06 12:18:22 -0500

    Text Modification
    This commit was to remove the original text and to add in new text.

--- hello-world.py
+++ hello-world.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #This program is to print "Hello World!" to stdout.
 
-#This is some sample text that I will remove later, for use in github.
+#This text replaces the initial text.
 
 print("Hello World!")
